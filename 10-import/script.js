2c7250d47ba425985a707cb29361db9c486edb20
Cresson Timothée <timotheecresson@hotmail.fr>
2023-10-21 18:57:56 +0200

    portfolio

---
+++ 10-import/script.js
@@ -0,0 +1,44 @@
+"use strict"
+/*
+=============================
+Animation Roue de compétence
+=============================
+*/
+
+const roue = document.querySelector('.roue');
+/**
+ * options pour l'observateur d'intersection
+ */
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.5, // Lorsque 50% de l'élément est visible
+};
+const observer = new IntersectionObserver(handleIntersect, options);
+// Fonction de callback à exécuter lorsque l'élément est visible
+function handleIntersect(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // L'élément est dans la vue, ajoutez la classe .rotate
+      roue.classList.add('rotate');
+    } else {
+      // L'élément n'est plus dans la vue, supprimez la classe .rotate
+      roue.classList.remove('rotate');
+    }
+  });
+}
+observer.observe(roue);
+
+/*
+=====================
+Animation navigation
+=====================
+*/
+const list = document.querySelectorAll(".list");
+function activeLink() {
+    list.forEach((item)=>
+    item.classList.remove('hover'));
+    this.classList.add('hover')
+}
+list.forEach((item)=>
+item.addEventListener('mousemove', activeLink))
