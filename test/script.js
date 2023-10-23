778bc6d100807d3ff37d96766b8785be52a7ba89
Cresson Timothée <timotheecresson@hotmail.fr>
2023-10-23 13:00:21 +0200

    portfolio fini

--- test/script.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-const list = document.querySelectorAll(".list");
-function activeLink() {
-    list.forEach((item)=>
-    item.classList.remove('active'));
-    this.classList.add('active')
-}
-list.forEach((item)=>
-item.addEventListener('click', activeLink))
