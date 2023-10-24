8310b83dbc57e9212fb5f23bad1698889a165f28
Timothée Cresson <timotheecresson@hotmail.fr>
2023-10-24 16:31:58 +0200

    dsqd

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -12,11 +12,11 @@ const roue = document.querySelector('.roue');
 const options = {
   root: null,
   rootMargin: '0px',
-  threshold: 0.5, // Lorsque 50% de l'élément est visible
+  threshold: 0.5 // Lorsque 50% de l'élément est visible
 };
-const observer = new IntersectionObserver(handleIntersect, options);
+const observer = new IntersectionObserver(tourneRoue, options);
 // Fonction de callback à exécuter lorsque l'élément est visible
-function handleIntersect(entries, observer) {
+function tourneRoue(entries) {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       // L'élément est dans la vue, ajoutez la classe .rotate
@@ -42,3 +42,31 @@ function activeLink() {
 }
 list.forEach((item)=>
 item.addEventListener('mousemove', activeLink))
+
+/*
+==========================
+Animation Title Experience
+==========================
+*/
+/*
+const persoExp = document.querySelector(".persoExp")
+const titleExp = document.querySelector(".titleExp")
+
+const option = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.8,
+}
+const observer2 = new IntersectionObserver(flipExp, option);
+function flipExp (entries) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      persoExp.style.transform = `rotate(45deg)`
+    } else {
+      persoExp.style.transform = ``
+    }
+  });
+}
+observer2.observe(persoExp);
+console.log(observer2);
+*/
